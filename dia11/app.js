f4cdc00580c5ce7e9a19c3d6dda933200d817fe7
Cris.Varon <100612787+elcr1s3@users.noreply.github.com>
2022-09-30 12:45:44 -0500

    Add files via upload

---
+++ dia11/app.js
@@ -0,0 +1,137 @@
+// CREATE A QUIZ CLASS
+class Quiz {
+    constructor(questions) {
+        this.score = 0;
+        this.questions = questions;
+        this.questionIndex = 0;
+    }
+
+    getQuestionIndex() {
+        return this.questions[this.questionIndex];
+    }
+
+    guess(answer) {
+        if (this.getQuestionIndex().isCorrectAnswer(answer)) {
+            this.score++;
+        }
+        this.questionIndex++;
+    }
+
+    isEnded() {
+        return this.questionIndex === this.questions.length;
+    }
+}
+
+// Create a question Class
+class Question {
+    constructor(text, choices, answer) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+    }
+
+    isCorrectAnswer(choice) {
+        return this.answer === choice;
+    }
+}
+
+// NOW DISPLAY THE QUESTIONS
+function displayQuestion() {
+    if (quiz.isEnded()) {
+        showScores();
+    } else {
+        // show question
+        let questionElement = document.getElementById("question");
+        questionElement.innerHTML = quiz.getQuestionIndex().text;
+
+        // show options
+        let choices = quiz.getQuestionIndex().choices;
+        for (let i = 0; i < choices.length; i++) {
+            let choiceElement = document.getElementById("choice" + i);
+            choiceElement.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+
+        showProgress();
+    }
+};
+
+// GUESS ANSWER
+function guess(id, guess) {
+    let button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        displayQuestion();
+    }
+};
+
+// SHOW QUIZ PROGRESS
+function showProgress() {
+    let currentQuestionNumber = quiz.questionIndex + 1;
+    let ProgressElement = document.getElementById("progress");
+    ProgressElement.innerHTML =
+        `Question ${currentQuestionNumber} of ${quiz.questions.length}`;
+};
+
+// SHOW SCORES
+function showScores() {
+    let quizEndHTML =
+        `
+    <h1>Listo?</h1>
+    <h2 id='score'> Lograste: ${quiz.score} de ${quiz.questions.length}</h2>
+    <div class="quiz-repeat">
+        <a href="index.html">Otra?></a>
+    </div>
+    `;
+    let quizElement = document.getElementById("quiz");
+    quizElement.innerHTML = quizEndHTML;
+};
+
+// create questions here
+let questions = [
+    new Question(
+        "pregunta 1?", ["seee", "puede", "simon", "nel"], "nel"
+    ),
+    new Question(
+        "pregunta 2?", ["seee", "puede", "simon", "nel"], "nel"
+    ),
+    new Question(
+        "pregunta 3?", ["seee", "puede", "simon", "nel"], "nel"
+    ),
+    new Question(
+        "pregunta 4?", ["seee", "puede", "simon", "nel"], "nel"
+    ),
+    new Question(
+        "pregunta 5?", ["seee", "puede", "simon", "nel"], "nel"
+    ),
+];
+
+// INITIALIZE quiz
+let quiz = new Quiz(questions);
+
+// display questions
+displayQuestion();
+
+
+// Add A CountDown for the Quiz
+let time = 10;
+let quizTimeInMinutes = time * 60 * 60;
+let quizTime = quizTimeInMinutes / 60;
+
+let counting = document.getElementById("count-down");
+
+function startCountdown() {
+    let quizTimer = setInterval(function() {
+        if (quizTime <= 0) {
+            clearInterval(quizTimer);
+            showScores();
+        } else {
+            quizTime--;
+            let sec = Math.floor(quizTime % 60);
+            let min = Math.floor(quizTime / 60) % 60;
+            counting.innerHTML = `TIME: ${min} : ${sec}`;
+        }
+    }, 1000);
+}
+
+startCountdown();
